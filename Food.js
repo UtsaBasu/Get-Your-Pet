a2ee92174ac7509ccaddfb14dcb4c6c9b8b88517
UtsaBasu <76468540+UtsaBasu@users.noreply.github.com>
2021-04-23 21:52:31 +0530

    Add files via upload

---
+++ Food.js
@@ -0,0 +1,53 @@
+class Food{
+  constructor(){
+    this.image = loadImage("Milk.png");
+    this.foodStock = foodStock;
+    this.foodStock = database.ref("food");
+    this.lastFed = lastFed;
+  }
+
+  getFoodStock() {
+    var foodRef = database.ref("food");
+    foodRef.on("value",function(data){
+    foodStock = data.val();
+    })
+  }
+
+  updateFoodStock(food) {
+    console.log("pinky")
+    database.ref("/").update({
+        food : food
+    })
+  }
+
+  deductFood(x) {
+    if(x <= 0) {
+        x = 0;
+      } else {
+        x--;
+      }
+    database.ref("/").update({
+        food: x
+    })
+  }
+
+  display(){
+    var x = 80
+    var y = 100;
+    imageMode(CENTER);
+
+    if(foodStock!==0) {
+      for(var i=0;i < foodStock;i++) {
+        if(i%35===0) {
+          x=80;
+          y=y+50;
+        }
+        image(this.image,x,y,50,50);
+        x = x+30;
+      }
+
+    }
+  
+  }
+}
+
